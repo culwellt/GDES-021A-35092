8ead96d592f81f3b7ec375b0c9e3246c44bd69f3
culwellt <tculwell99@yahoo.com>
2025-04-24 21:53:11 -0700

    .

---
+++ JS.2/main.js
@@ -0,0 +1,20 @@
+// declare a variable using the keyword "let" and the identifier aka the name of "cats" and assign the value of 22
+let cats;
+cats = 22;
+// declare a variable using the identifier aka the name of "dogs" and assign the value of 13
+let dogs;
+dogs = 13;
+// declare a variable using the identifier aka the name of "addingVariables" and assign the expression of "cats + dogs"
+let addingVariables;
+addingVariables = cats + dogs;
+// declare a variable using the identifier aka the name of "addingStrings" and concat 'javascript is ' + 'really awesome!'
+let addingStrings;
+addingStrings = "javascript is " + "really awesome!";
+// declare an array using the identifier aka the name of "myArray" and add these three items into it: 'Coding is awesome', 500, false
+let myArray;
+myArray = ["Coding is awesome", 500, false];
+// resassign myArray[1] to 250
+myArray[1] = 250;
+// create your own variable and assign a random string/message to it
+let myVariable;
+myVariable = "this is fun!";
